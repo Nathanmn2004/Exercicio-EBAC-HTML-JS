88209c8a7bd83bb09a474d743016a84dce8ea2d7
Nathan <nathanmeira@teste.gmail>
2024-11-29 11:37:24 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+const valor_1 = document.getElementById("primeiro-numero");
+const valor_2 = document.getElementById("segundo-numero");
+const form = document.getElementById("formulario");
+
+
+form.addEventListener("submit", function(e){
+    
+})
+
+
