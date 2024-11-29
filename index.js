ab2d081ecafa93f4930961406252e5127f166e0c
Nathan <nathanmeira@teste.gmail>
2024-11-29 11:58:14 -0300

    Edicoes terminadas branch secundaria

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
 
-
-
 const valor_1 = document.getElementById("primeiro-numero");
 const valor_2 = document.getElementById("segundo-numero");
 const form = document.getElementById("formulario");
+const p = document.querySelector("p");
 
 
 form.addEventListener("submit", function(e){
-    
+    e.preventDefault();
+
+    if(valor_1.value > valor_2.value){
+        p.textContent = "O primeiro Valor é maior que o primeiro";
+        p.classList.add("editar-p");
+        p.style.padding ="15px"
+    }else if(valor_2.value > valor_1.value){
+        p.textContent = "O segundo valor é maior que o Primeiro";
+        p.classList.add("editar-p");
+        p.style.padding ="15px"
+    }else{
+        p.textContent = "Os valores são iguais";
+        p.classList.add("editar-p");
+        p.style.padding ="15px"
+    }
+
 })
 
 
