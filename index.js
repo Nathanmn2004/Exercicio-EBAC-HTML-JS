93eb891e694a47de1bec7b7555d46317e7a786ab
Nathan <nathanmeira@teste.gmail>
2024-11-30 12:46:48 -0300

    Edicao Funcional

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 
 
-const valor_1 = document.getElementById("primeiro-numero");
-const valor_2 = document.getElementById("segundo-numero");
+
 const form = document.getElementById("formulario");
 const p = document.querySelector("p");
 
 
 form.addEventListener("submit", function(e){
+
+    const valor_1 = parseFloat(document.getElementById("primeiro-numero").value);
+    const valor_2 = parseFloat(document.getElementById("segundo-numero").value);
     e.preventDefault();
 
-    if(valor_1.value > valor_2.value){
+    if(isNaN(valor_1) || isNaN(valor_2)){
+        p.textContent = "Digite apenas Números";
+        p.classList.add("editar-p");
+        p.style.padding = "15px";
+        return;
+    }
+    
+    if(valor_1 > valor_2){
         p.textContent = "O primeiro Valor é maior que o segundo";
         p.classList.add("editar-p");
         p.style.padding ="15px"
-    }else if(valor_2.value > valor_1.value){
+    }else if(valor_2 > valor_1){
         p.textContent = "O segundo valor é maior que o Primeiro";
         p.classList.add("editar-p");
         p.style.padding ="15px"
